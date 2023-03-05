b57a05ac3e24f2f144bf707087712f2125346f0a
Felix-fale <falefelix23@gmail.com>
2023-03-05 22:11:21 +0000

    feat: add Forms component content

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
 import FormItem from "./FormItem";
 
 function Forms(props) {
+  // store index number with the answers?
+  const [answers, setAnswers] = useState({ index: props.step });
+
+  useEffect(() => {
+    // check if the answers isn't empty
+    if (Object.keys(answers).length > 1) {
+      // update page answers
+      props.onPageUpdate(answers.index, answers);
+      // update page number locally
+      setAnswers({ index: props.step });
+    } else {
+      // update page number locally
+      setAnswers({ index: props.step });
+    }
+  }, [props.step]);
+
+  const updateAnswers = (value, category) => {
+    setAnswers({ ...answers, [category]: value });
+  };
+
   return (
-    <>
-      <div className="text-left">
-        {props.list[0].items?.map((item, index) => {
-          return (
-            <>
-              <div>{item.label} </div>
-              <div>{item.type} </div>
-            </>
-          );
-        })}
-      </div>
-    </>
+    <div className="text-left">
+      {props.list[props.step - 1].items?.map((item, index) => {
+        return (
+          <FormItem
+            key={`${index}_${item.label}`}
+            item={item}
+            onChange={updateAnswers}
+            answer={
+              props.pagesAnswers[props.step]
+                ? props.pagesAnswers[props.step][item.value]
+                : null
+            }
+          />
+        );
+      })}
+    </div>
   );
 }
 
